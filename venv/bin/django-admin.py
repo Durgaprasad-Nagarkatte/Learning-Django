99ea637e27a023a0318cdcc6c310b37de18c4e6f
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2019-07-08 15:49:08 +0530

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/durgaprasad/Desktop/June/learning_django/helloworld/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
